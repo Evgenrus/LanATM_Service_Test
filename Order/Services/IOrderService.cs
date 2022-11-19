48a47c86d3afa9c99889de5ebe19c75e77a2f689
Evgen <evgenrus257@yandex.ru>
2022-11-19 18:54:05 +0700

    Service added

---
+++ Order/Services/IOrderService.cs
@@ -0,0 +1,17 @@
+using System;
+using Order.Models;
+
+namespace Order.Services;
+public interface IOrderService
+{
+    public Task<CartModel> GetCartById(int id);
+    public Task<ICollection<CartModel>> GetCartsByCustomerId(int id);
+    public Task<CustomerModel> GetCustomerByLogin(string Login);
+    public Task<OrderModel> GetOrderById(int id);
+    public Task<ICollection<OrderModel>> GetOrdersByCustomerId(int id);
+    public Task<OrderModel> PostOrder(OrderModel model, int customerId);
+    public Task<CartModel> NewCart(int customerId);
+    public Task<CartModel> AddItemToCart(ItemModel model, int cartId, int customerId);
+    public Task CancelOrder(int orderId);
+    public Task<int> PostNewCustomer(CustomerModel model);
+}
