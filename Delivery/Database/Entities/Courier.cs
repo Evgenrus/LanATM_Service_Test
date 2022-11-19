011795d36644564b20ab90b9b171f8e0e1f7c1d2
Евгений <evgenrus257@yandex.ru>
2022-11-19 20:24:23 +0700

    Entities added

---
+++ Delivery/Database/Entities/Courier.cs
@@ -0,0 +1,14 @@
+﻿namespace Delivery.Database.Entities;
+
+public class Courier
+{
+    public int Id { get; set; }
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Phone { get; set; }
+    public string Mail { get; set; }
+    public string Photo { get; set; }
+
+    public List<OrderDelivery> Deliveries { get; set; }
+}
---
+++ Delivery/Database/Entities/OrderDelivery.cs
@@ -0,0 +1,22 @@
+﻿namespace Delivery.Database.Entities;
+
+internal enum Status : int
+{
+    Pending,
+    Assigned,
+    InProcess,
+    Delivered
+}
+
+public class OrderDelivery
+{
+    public int Id { get; set; }
+    
+    public int? CustomerId { get; set; }
+    public Customer? Customer { get; set; }
+    
+    public int? CourierId { get; set; }
+    public Courier? Courier { get; set; }
+
+    public List<DeliveryItem> Items { get; set; } = new List<DeliveryItem>();
+}
