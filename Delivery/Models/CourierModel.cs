fa4cc4d8bacde7fd5933827a02e8ed44dbe8053f
Евгений <evgenrus257@yandex.ru>
2022-11-20 17:53:49 +0700

    Added models

---
+++ Delivery/Models/CourierModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Delivery.Models;
+
+public class CourierModel
+{
+    public int Id { get; set; }
+    public string Login { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Phone { get; set; }
+    public string Mail { get; set; }
+    public string Photo { get; set; }
+}
